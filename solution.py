3d93b0955c3b97136109ea3d8ab4bff0d27db4b5
Adamniels <adamnielsen401@gmail.com>
2025-09-17 17:08:52 +0200

    some start work

---
+++ solution.py
@@ -0,0 +1,23 @@
+
+c = 0.05 # mass
+Km = 700 # m/s
+m0 = 8 # startmassa
+g = 9.81 # gravity
+
+
+
+def Fvec(t):
+    f1 = m(t)*
+    f2 = m(t)*
+
+def m(t):
+    if (t <= 10):
+        return 8.0 - 0.4t
+    else if( t > 10):
+        return 4
+
+def mprim(t):
+    if (t <= 10):
+        return -0.4
+    else if( t > 10):
+        return 0
